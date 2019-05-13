36d137fb9f663a8c56e98e7944aea173c302522b
PBeani <bunnyzones25@gmail.com>
2019-05-12 22:13:32 -0300

    Criando rep

---
+++ Assets/PlayerColision.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerColision : MonoBehaviour {
+
+    int damage;
+
+    void start() {
+        this.damage = 0;
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.name.Contains("pessoa")) {
+            updateDamage(10);
+        }
+    }
+
+    void OnTriggerStay(Collider other) {
+        if(other.gameObject.name.Contains("pessoa")) {
+            updateDamage(1);
+        }
+
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.gameObject.name.Contains("pessoa")) {
+        }
+    }
+
+    void updateDamage(int value) {
+        damage += value;
+        GameObject placar = GameObject.Find("placar");
+        placar.gameObject.GetComponent<Text>().text = this.damage.ToString();
+    }
+}
---
+++ Assets/scripts/PlayerControllerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerMovement : MonoBehaviour
+{
+    private string moveInputAxis = "Vertical";
+    private string rotateInputAxis = "Horizontal";
+
+    public float rotationRate = 270;
+    public float moveRate = 10;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float moveAxis = Input.GetAxis(moveInputAxis);
+        float rotateAxis = Input.GetAxis(rotateInputAxis);
+
+        ApplyInput(moveAxis, rotateAxis);
+    }
+
+    private void ApplyInput(float move, float rotate)
+    {
+        Move(move);
+        Turn(rotate);
+    }
+
+    private void Move(float input) {
+        if(input < 0) {
+            input = 0;
+        }
+
+        rb.AddForce(transform.forward * input * moveRate);
+    }
+
+    private void Turn(float input) {
+        transform.Rotate(0, input * rotationRate * Time.deltaTime, 0);
+    }
+}
